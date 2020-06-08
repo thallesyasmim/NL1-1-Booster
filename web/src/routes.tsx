0b7166ec81933fb91b6d76ac83e26f2124ce72cd
Thalles Gabriel Lima Lossurdo <thallesgabriel1307@gmail.com>
2020-06-08 19:24:02 -0300

    NLW

---
+++ web/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+// exact: ele vai verificar
+
+const Routes =  () => {
+    return(
+        <BrowserRouter>
+            <Route component={Home} path="/" exact /> 
+            <Route component={CreatePoint} path="/create-point" />
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
